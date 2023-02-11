38abed8dcc1390a1f8ee44dfbd181c26a10fba79
Dumitru <cusnir.dumitru@gmail.com>
2023-02-11 20:59:20 +0200

    Gitignore + first deploy

--- src/app/question-bank-edit/question-import/question-import.component.ts
+++ src/app/question-bank-edit/question-import/question-import.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { QuestionBankService } from "../../services/question-bank.service";
 import { ActivatedRoute } from "@angular/router";
