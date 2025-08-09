8aa6306627b984df449bf8f3f09386eeac476c2d
Dumitru <cusnir.dumitru@gmail.com>
2025-08-09 21:24:22 +0300

    working routing

--- apps/aqb/src/main.ts
+++ apps/aqb/src/main.ts
@@ -49,8 +49,27 @@ const routes = [
         component: StatisticsComponent
     },
     {
-        path: ":id",
-        loadComponent: () => import("./app/features/question-bank/question-bank-edit/question-bank-edit.component").then(m => m.QuestionBankEditComponent)
+        path: "banks/:id",
+        loadComponent: () => import("./app/features/question-bank/question-bank-edit/question-bank-edit.component").then(m => m.QuestionBankEditComponent),
+        children: [
+            {
+                path: "questions",
+                loadComponent: () => import("./app/features/question-bank/question-bank-edit/question-list-edit/question-list-edit.component").then(m => m.QuestionListEditComponent)
+            },
+            {
+                path: "add",
+                loadComponent: () => import("./app/features/question-bank/question-bank-edit/question-add/question-add.component").then(m => m.QuestionAddComponent)
+            },
+            {
+                path: "import",
+                loadComponent: () => import("./app/features/question-bank/question-bank-edit/question-import/question-import.component").then(m => m.QuestionImportComponent)
+            },
+            {
+                path: "",
+                redirectTo: "questions",
+                pathMatch: "full" as const
+            }
+        ]
     }
 ];
 
