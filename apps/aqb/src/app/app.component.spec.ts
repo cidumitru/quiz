147eb9265e2d665c570a27f0f9b718567f5b2698
Dumitru <cusnir.dumitru@gmail.com>
2025-08-16 11:58:16 +0300

    mock test for fe

---
+++ apps/aqb/src/app/app.component.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'router-outlet',
+  template: '',
+})
+class MockRouterOutlet {}
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    })
+      .overrideComponent(AppComponent, {
+        remove: { imports: [RouterOutlet] },
+        add: { imports: [MockRouterOutlet] },
+      })
+      .compileComponents();
+  });
+
+  it('should create', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  it('should render router-outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
+});
