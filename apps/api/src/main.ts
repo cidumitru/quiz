58f3892800337aa7d003aeb12d402f20ee9d2b82
Dumitru <cusnir.dumitru@gmail.com>
2025-08-23 20:32:59 +0300

    explicit ipˆ

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   app.enableShutdownHooks();
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
